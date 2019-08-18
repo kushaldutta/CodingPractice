1697e2be2ee95b42ce002b6aff2d7e81575b1e6a
Kushal Dutta <kushaldutta1749@gmail.com>
2019-08-17 23:38:31 -0700

    made comments more readible

--- Waterweigher.py
+++ Waterweigher.py
@@ -1,15 +1,15 @@
-#repeat is set to 'yes' for while loop to start
+# repeat is set to 'yes' for while loop to start
 repeat = 'yes'
 
 while repeat.lower() == 'yes':
-    #asks user for amount of gallons
+    # asks user for amount of gallons
     gal = float(input("Enter the amount of water in gallons: "))
 
-    #converts gallons to pounds
+    # converts gallons to pounds
     weight = gal * 8.34
 
-    #prints the weight of the water in pounds
+    # prints the weight of the water in pounds
     print(str(weight))
 
-    #asks user to repeat
+    # asks user to repeat
     repeat = input("Enter 'yes' if you would like to repeat: ")
