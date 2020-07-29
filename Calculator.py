835820ec5f21f7edbb3bb342a3cccf32aacbac65
Kushal Dutta <kushaldutta1749@gmail.com>
2020-07-29 09:41:00 -0700

    functional calculator

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+def add(x, y):
+    return x+y
+def sub(x, y):
+    return x-y
+def mult(x, y):
+    return x*y
+def div(x, y):
+    return x/y
+
+
+def calc(operation, x, y):
+    return operation(x, y)
+
