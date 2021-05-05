4fdc88c59f64822a6c54387f7cb3088f44cf9b82
Kushal Dutta <kushaldutta1749@gmail.com>
2021-05-04 18:31:28 -0700

    fewer lines by using methods

--- Java/SimpleCalculator.java
+++ Java/SimpleCalculator.java
@@ -1,98 +1,72 @@
 import java.util.Scanner;
 
 public class SimpleCalculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static double add(double num1) {
+        System.out.println("Addition Format: a + b");
+        System.out.print("Enter the second value (b): ");
+        double num2 = scanner.nextDouble();
+        double result = num1 + num2;
+        return result;
+    }
+    
+    public static double subtract(double num1) {
+        System.out.println("Subtraction Format: a - b");
+        System.out.print("Enter the second value (b): ");
+        double num2 = scanner.nextDouble();
+        double result = num1 - num2;
+        return result;
+    }
+    
+    public static double multiply(double num1) {
+        System.out.println("Multiplcation Format: a * b");
+        System.out.print("Enter the second value (b): ");
+        double num2 = scanner.nextDouble();
+        double result = num1 * num2;
+        return result;
+        
+    }
+    
+    public static double divide(double num1) {
+        System.out.println("Division Format: a / b");
+        System.out.print("Enter the second value (b): ");
+        double num2 = scanner.nextDouble();
+        double result = num1 / num2;
+        return result;
+    }
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double input1 = 0, input2 = 0, a, b, addResult, subResult, mulResult, divResult;
-        int operation;
-        String advance;
-        do {    
-            do {
-                System.out.print("Enter '1' for addition, '2' for subtraction, '3' for multiplication, or '4' for division: ");
-                operation = scanner.nextInt();
-                    if (operation == 1) {
-                        System.out.println("Addition Format: a + b");
-                        System.out.print("Enter the first value (a): ");
-                        input1 = scanner.nextDouble();
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        addResult = input1 + input2;
-                        input1 = addResult;
-                        System.out.println("Result: " + addResult);
-                    }
-                    else if (operation == 2) {
-                        System.out.println("Subtraction Format: a - b");
-                        System.out.print("Enter the first value (a): ");
-                        input1 = scanner.nextDouble();
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        subResult = input1 - input2;
-                        input1 = subResult;
-                        System.out.println("Result: " + subResult);
-                    }
-                    else if (operation == 3) {
-                        System.out.println("Multiplacation Format: a * b");
-                        System.out.print("Enter the first value (a): ");
-                        input1 = scanner.nextDouble();
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        mulResult = input1 * input2;
-                        input1 = mulResult;
-                        System.out.println("Result: " + mulResult);
-                    }
-                    else if (operation == 4) {
-                        System.out.println("Division Format: a/b");
-                        System.out.print("Enter the first value (a): ");
-                        input1 = scanner.nextDouble();
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        divResult = input1 / input2;
-                        input1 = divResult;
-                        System.out.println("Result: " + divResult);
-                    }
-                    scanner.nextLine();
-                    System.out.print("Enter '1' if you want to continue with this value or '2' if you want to reset: ");
-                    advance = scanner.nextLine();
-                } while (advance.equals("2"));
-            do {
-                System.out.print("Enter '1' for addition, '2' for subtraction, '3' for multiplication, or '4' for division: ");
-                operation = scanner.nextInt();
-                    if (operation == 1) {
-                        System.out.println("Addition Format: a + b (a = " + input1 + ")");
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        addResult = input1 + input2;
-                        input1 = addResult;
-                        System.out.println("Result: " + addResult);
-                    }
-                    else if (operation == 2) {
-                        System.out.println("Subtraction Format: a - b (a = " + input1 + ")");
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        subResult = input1 - input2;
-                        input1 = subResult;
-                        System.out.println("Result: " + subResult);
-                    }
-                    else if (operation == 3) {
-                        System.out.println("Multiplacation Format: a * b (a = " + input1 + ")");
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        mulResult = input1 * input2;
-                        input1 = mulResult;
-                        System.out.println("Result: " + mulResult);
-                    }
-                    else if (operation == 4) {
-                        System.out.println("Division Format: a/b (a = " + input1 + ")");
-                        System.out.print("Enter the second value (b): ");
-                        input2 = scanner.nextDouble();
-                        divResult = input1 / input2;
-                        input1 = divResult;
-                        System.out.println("Result: " + divResult);
-                    }
-                    scanner.nextLine();
-                    System.out.print("Enter '1' if you want to continue with this value or '2' if you want to reset: ");
-                    advance = scanner.nextLine();
-                } while (advance.equals("1"));
-        } while (advance.equals("2"));
+        int advance, operation;
+        double  input;
+        do {
+            
+        System.out.print("What is your first value? ");
+        input = scanner.nextDouble();
+        
+        do {
+            System.out.print("Enter '1' for addition, '2' for subtraction, '3' for multiplication, or '4' for division: ");
+            operation = scanner.nextInt();
+            
+            switch (operation) {
+                case 1:
+                    input = add(input);
+                    break;
+                case 2:
+                    input = subtract(input);
+                    break;
+                case 3:
+                    input = multiply(input);
+                    break;
+                case 4:
+                    input = divide(input);
+                    break;
+            }
+            System.out.println("Result: " + input);
+            System.out.print("Enter '1' if you want to continue with this value or '2' if you want to reset: ");
+            advance = scanner.nextInt();
+        } while (advance == 1);
+        
+        } while (advance == 2);
     }
 }
