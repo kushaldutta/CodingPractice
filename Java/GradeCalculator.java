b95b5d7d274b92f187807e71b7f57b12b2bbe427
Kushal Dutta <kushaldutta1749@gmail.com>
2021-06-06 11:00:18 -0700

    put grades in array

--- Java/GradeCalculator.java
+++ Java/GradeCalculator.java
@@ -3,72 +3,94 @@
 import java.util.Scanner;
 
 public class GradeCalculator {
+    
     static Scanner scanner = new Scanner(System.in);
-    static int advance = 1, amountOfCategories = 0;
-    static double totalWeight = 0;
+    static int amountOfCategories;
+    static double[] grades;
+    static double[] weight;
+    
     public static void main(String[] args) {
-        double grade;
-        double sub1 = 0, sub2 = 0, sub3 = 0, sub4 = 0, sub5 = 0;
-               
-        sub1 = subPercentage(); 
-        if (advance == 1)
-            sub2 = subPercentage(); 
-        if (advance == 1)
-            sub3 = subPercentage(); 
-        if (advance == 1)
-            sub4 = subPercentage();
-        if (advance == 1)
-            sub5 = subPercentage();
-        
-        grade = (sub1 + sub2 + sub3 + sub4 + sub5 ) / ( totalWeight / 100 );
-        System.out.printf("Your grade is %.3f\n", grade);
+        GradeCalculator Freshman = new GradeCalculator();
+        Freshman.Percentage();
+        System.out.printf("%nYour grade is %.3f", Freshman.Calculate());
+        System.out.println("%");
     }
     
-    public static double subPercentage() {
-        amountOfCategories++;
-        if (amountOfCategories > 1)
-            advance = advance();
-        if (advance == 2) 
-            return 0; 
-       
-        double weight = weight();
-        System.out.print("How many points are in this category? ");
-        double total = scanner.nextDouble();
-        if (total == 0)
-            total = 1;
-        
-        System.out.print("How many points have you earned in this category? ");
-        double earned = scanner.nextDouble();
-        double percent = earned / total * weight;
-        return percent;
+    /**
+     * Default Constructor asks for amount of assignment categories
+     */
+    public GradeCalculator() {
+        System.out.print("Enter the amount of categories: ");
+        amountOfCategories = scanner.nextInt();
+        grades = new double[amountOfCategories];
+        weight = new double[amountOfCategories];
+    }
+      
+    /**
+     * Percentage method gets percentage and weight of each assignment category
+     */
+    public void Percentage() {
+        for (int i = 0; i < grades.length; i++) {
+            double totalPoints;
+            double earnedPoints;
+            
+            System.out.println("\nCategory #" + (i + 1));
+            
+            System.out.print("How many total points are in this category? ");
+            totalPoints = scanner.nextDouble();
+            
+            System.out.print("How many points have you earned in this category? ");
+            earnedPoints = scanner.nextDouble();
+            
+            System.out.print("What percentage is this category worth? ");
+            weight[i] = scanner.nextDouble();
+            
+            grades[i] = earnedPoints / totalPoints;
+        }
     }
     
-    public static int advance() {
-        System.out.print("Type 1 to continue or 2 if there are no more categories: ");
-        advance = scanner.nextInt(); 
-        return advance;
+    /**
+     * Calculate method find total worth of one assignment category
+     * @return subPercent total percentage earned from one assignment category
+     */
+    public double Calculate() {
+        double subPercent = 0;
+        for (int i = 0; i < grades.length; i++) {
+            subPercent += (grades[i] * 100 * weight[i]) / totalWeight();
+        }
+        return subPercent;
     }
     
-    public static double weight() {
-        System.out.print("What percentage is this category worth? ");
-        double weight = scanner.nextDouble();
-        totalWeight += weight;
-        return weight;
+    /**
+     * totalWeight method adds the weight of all categories
+     * @return totalWeight sum of weights
+     */
+    public double totalWeight() {
+        double totalWeight = 0;
+        for (int i = 0; i < weight.length; i++) {
+            totalWeight += weight[i];
+        }
+        return totalWeight;
     }
 }
 
 /*
-What percentage is this category worth? 10
-How many points are in this category? 76
-How many points have you earned in this category? 73
-Type 1 to continue or 2 if there are no more categories: 1
+Enter the amount of categories: 3
+
+Category #1
+How many total points are in this category? 40
+How many points have you earned in this category? 35
+What percentage is this category worth? 20
+
+Category #2
+How many total points are in this category? 10
+How many points have you earned in this category? 7.8
 What percentage is this category worth? 30
-How many points are in this category? 40
-How many points have you earned in this category? 35.67
-Type 1 to continue or 2 if there are no more categories: 1
-What percentage is this category worth? 60
-How many points are in this category? 16
-How many points have you earned in this category? 15
-Type 1 to continue or 2 if there are no more categories: 2
-Your grade is 92.608
+
+Category #3
+How many total points are in this category? 50
+How many points have you earned in this category? 48
+What percentage is this category worth? 50
+
+Your grade is 88.900%
 */
